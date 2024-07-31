36feec2554ab48f46ae1294deb6c0878d6fee1e5
malshan_r <malshan.edu@gmail.com>
2024-08-01 01:15:36 +0530

    init proj

---
+++ online-code-editor/Controllers/AnalyzerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace online_code_editor.Controllers
+{
+    public class AnalyzerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CheckSyntax([FromBody] string code)
+        {
+            var syntaxTree = CSharpSyntaxTree.ParseText(code);
+            var diagnostics = syntaxTree.GetDiagnostics();
+
+            var errors = diagnostics.Where(d => d.Severity == DiagnosticSeverity.Error)
+                                    .Select(d => d.GetMessage());
+
+            return Ok(new { errors = errors.ToArray() });
+        }
+    }
+}
